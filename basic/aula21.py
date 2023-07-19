1f1b9cc2b06fc6685cb2d1e4bf15c57a576e4eed
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-07-19 20:18:41 -0300

    feat: learned about 'and' logical operator

---
+++ basic/aula21.py
@@ -0,0 +1,13 @@
+# entrada = input('[E]ntrar [S]air: ')
+# senha_digitada = input('Senha: ')
+
+# senha_permitida = '123456'
+
+# if entrada == 'E' and senha_digitada == senha_permitida:
+#     print('Entrar')
+# else:
+#     print('Sair')
+
+# Avaliação de curto circuito
+print(True and False and True)
+print(True and 0 and True)
