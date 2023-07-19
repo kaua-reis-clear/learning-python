7d16e5d8bfbf93039c0638f08ed16e5d5ba50fd0
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-07-18 23:59:27 -0300

    feat: variables

---
+++ basic/aula7.py
@@ -0,0 +1,13 @@
+#variables
+
+# nome_completo = 'Kauã dos Reis'
+# soma_dois_mais_dois = 2 + 2
+# int_um = bool('1')
+# print(int_um, type(int_um))
+# print(nome_completo, soma_dois_mais_dois)
+
+nome = 'Kauã'
+idade = 18
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
