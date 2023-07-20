eb64b64203122259af2bff24881fc9e59be035f6
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-07-20 19:27:30 -0300

    feat: learned about insert method

--- basic/aula48.py
+++ basic/aula48.py
@@ -1,12 +1,8 @@
-#        0   1   2   3   4   5
 lista = [10, 20, 30, 40]
-# lista[2] = 300
-# del lista[2]
-# print(lista)
-# print(lista[2])
-lista.append(50)
-lista.pop()
-lista.append(60)
-lista.append(70)
-ultimo_valor = lista.pop(3)
-print(lista, 'Removido,', ultimo_valor)
+lista.append('Kauã')
+nome = lista.pop()
+lista.append(1233)
+del lista[-1]
+# lista.clear()
+lista.insert(100, 5)
+print(lista[4])
