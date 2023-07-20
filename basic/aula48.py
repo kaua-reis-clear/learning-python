825ec02f2139eaac1f26618a303a06ffb8665856
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-07-20 19:49:13 -0300

    feat: learned about copy method

--- basic/aula48.py
+++ basic/aula48.py
@@ -1,5 +1,6 @@
-lista_a = [1, 2, 3]
-lista_b = [4, 5, 6]
-lista_c = lista_a + lista_b
-lista_a.extend(lista_b)
+lista_a = ['Luiz', 'Maria', 1, True, 1.2]
+lista_b = lista_a.copy()
+
+lista_a[0] = 'Qualquer coisa'
 print(lista_a)
+print(lista_b)
