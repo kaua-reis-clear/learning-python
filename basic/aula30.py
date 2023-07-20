c30d3e6d0f6d78e19ced4026d671a0814d35d70d
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-07-19 21:11:15 -0300

    feat: learned about variable and constant conventions

---
+++ basic/aula30.py
@@ -0,0 +1,20 @@
+velocidade = 61 # variable
+local_carro = 100 # variable
+
+RADAR_1 = 60 # CONSTANT
+LOCAL_1 = 100 # CONSTANT
+RADAR_RANGE = 1 # CONSTANT
+
+vel_carro_pass_radar_1 = velocidade > RADAR_1
+carro_passou_radar_1 = local_carro >= (LOCAL_1 - RADAR_RANGE) and \
+    local_carro <= (LOCAL_1 + RADAR_RANGE)
+carro_multado_radar_1 = carro_passou_radar_1 and velocidade > RADAR_1
+
+if vel_carro_pass_radar_1:
+    print('Velocidade carro passou do radar 1')
+
+if carro_passou_radar_1:
+    print('Carro passou radar 1')
+
+if carro_multado_radar_1:
+    print('carro multado em radar 1')
