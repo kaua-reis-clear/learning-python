f6e157c32a268f5ddb5a87afa039a44481575c30
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-07-20 19:02:15 -0300

    feat: learned about iterables

---
+++ basic/aula45.py
@@ -0,0 +1,13 @@
+texto = 'Kauã'  # iterável
+
+# iteratador = iter(texto)  # iterator
+
+# while True:
+#     try:
+#         letra = next(iteratador)
+#         print(letra)
+#     except StopIteration:
+#         break
+
+for letra in texto:
+    print(letra)
