732053f617ed3f7653ad2a0e2fd8a193f71f78be
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-07-19 22:29:00 -0300

    feat: learned about assignment operators

---
+++ basic/aula36.py
@@ -0,0 +1,15 @@
+"""
+Operadores de atribuição
+= += -= *= /= //= **= %=
+"""
+
+contador = 10
+
+# while contador > 10:
+#     contador = contador + 1
+#     print(contador)
+
+# print('Acabou')
+
+contador /= 5
+print(contador)
