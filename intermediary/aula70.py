71132bbd489e5cd9fe751abd0086ea71c399d4bc
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-07-25 20:30:29 -0300

    feat: learned about functions with return

---
+++ intermediary/aula70.py
@@ -0,0 +1,13 @@
+def sum(x, y):
+    if x > 10:
+        return [10, 20]
+    return x + y
+
+
+# variable = sum(1, 2)
+# variable = int('1')
+sum1 = sum(2, 2)
+sum2 = sum(3, 3)
+print(sum1)
+print(sum2)
+print(sum(11, 55))
