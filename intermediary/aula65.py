c913fe72c5d278fe13986f74cd4909b1c9feb3e1
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-07-25 19:28:07 -0300

    feat: learned about functions

---
+++ intermediary/aula65.py
@@ -0,0 +1,21 @@
+# def Print(a, b, c):
+#     print('Várias1')
+#     print('Várias2')
+#     print('Várias3')
+#     print('Várias4')
+
+# def imprimir(a, b, c):
+#     print(a, b, c)
+
+
+# imprimir(1, 2, 3)
+# imprimir(4, 5, 6)
+
+def saudacao(nome='Sem nome'):
+    print(f'Olá, {nome}!')
+
+
+saudacao('Luiz Otávio')
+saudacao('Maria')
+saudacao('Helena')
+saudacao()
