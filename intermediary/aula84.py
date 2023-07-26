216409984c90e50b660793dc1345043bb7f36fe9
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-07-25 23:57:59 -0300

    feat: mapping data in list comprehension

--- intermediary/aula84.py
+++ intermediary/aula84.py
@@ -1,12 +1,30 @@
 my_list = []
 
 for number in range(10):
-    list.append(number)
+    my_list.append(number)
     
 # print(my_list)
 
 my_list = [
-    number for number in range(10)
+    number * 2
+    for number in range(10)
 ]
 
+print(list(range(10)))
 print(my_list)
+
+# Mapping data in list comprehension
+products = [
+    {'name': 'p1', 'price': 20,},
+    {'name': 'p2', 'price': 10,},
+    {'name': 'p3', 'price': 30,},
+]
+
+new_products = [
+  {**product, 'price': product['price'] * 1.05}
+  if product['price'] > 20 else {**product}
+  for product in products
+]
+
+print(new_products)
+print(*new_products, sep='\n')
