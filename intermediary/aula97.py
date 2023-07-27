c7391f23dfc2274a0d34774312424c855968c177
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-07-26 21:47:45 -0300

    feat: learned how to import modules properly

---
+++ intermediary/aula97.py
@@ -0,0 +1,9 @@
+import aula97_m
+
+from aula97_m import my_sum, module_variable
+
+# print('This module is the ', __name__)
+print(aula97_m.module_variable)
+print(module_variable)
+print(my_sum(2, 3))
+print(aula97_m.my_sum(2, 3))
---
+++ intermediary/aula97_m.py
@@ -0,0 +1,5 @@
+module_variable = 'Luiz'
+
+
+def my_sum(x, y):
+    return x + y
