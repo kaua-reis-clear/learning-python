ff55f504cda869fdf05b9b677dc698d9f679b1ea
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-07-30 16:58:09 -0300

    feat: learned more about recursive functions

--- intermediary/aula114.py
+++ intermediary/aula114.py
@@ -1,15 +1,31 @@
-def recursive(start=0, end=4):
+# import sys
 
-    print(start, end)
-
-    # Base case
-    if start >= end:
-        return end
-
-    # Recursive case
-    # count until the end is reached  
-    start += 1
-    return recursive(start, end)
+# sys.setrecursionlimit(1004)
 
 
-print(recursive())
+# def recursive(start=0, end=4):
+
+#     print(start, end)
+
+#     # Base case
+#     if start >= end:
+#         return end
+
+#     # Recursive case
+#     # count until reach the end
+#     start += 1
+#     return recursive(start, end)
+
+
+# print(recursive(0, 1001))
+
+def factorial(n):
+    if n <= 1:
+        return 1
+
+    return n * factorial(n - 1)
+
+
+print(factorial(5))
+print(factorial(10))
+print(factorial(100))
