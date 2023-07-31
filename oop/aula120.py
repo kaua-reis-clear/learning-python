f820f0c332123872cc65117a37361d8086fc9d26
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-07-31 20:55:59 -0300

    feat: learned about classes

---
+++ oop/aula120.py
@@ -0,0 +1,17 @@
+class Person:
+    ...
+
+
+p1 = Person('Kauã', 'Drar')
+p1.first_name = 'Kauã'
+p1.last_name = 'Drar'
+
+p2 = Person('Vitória', 'Rossi')
+p2.first_name = 'Vitória'
+p2.last_name = 'Rossi'
+
+print(p1.first_name)
+print(p1.last_name)
+
+print(p2.first_name)
+print(p2.last_name)
