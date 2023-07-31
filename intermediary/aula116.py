6c0ca07a9d3cfc819d2cf0eab1dd1a56d683d796
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-07-31 16:30:48 -0300

    feat: learned about file opening behaviors and encoding using with open

--- intermediary/aula116.py
+++ intermediary/aula116.py
@@ -21,27 +21,36 @@ file_path = 'aula116.txt'
 # file = open(file_path, 'w')
 # #
 # file.close()
-with open(file_path, 'w+') as file:
+
+# with open(file_path, 'w+') as file:
+#     file.write('Line 1\n')
+#     file.write('Line 2\n')
+#     file.writelines(
+#         ('Line 3\n', 'Line 4\n')
+#     )
+#     file.seek(0, 0)
+#     print(file.read())
+#     print('Reading')
+#     file.seek(0, 0)
+#     print(file.readline(), end='')
+#     print(file.readline().strip())
+#     print(file.readline().strip())
+
+#     print('READLINES')
+#     file.seek(0, 0)
+#     for line in file.readlines():
+#         print(line.strip())
+
+
+# print('#' * 10)
+
+# with open(file_path, 'r') as file:
+#     print(file.read())
+
+with open(file_path, 'w', encoding='utf8') as file:
+    file.write('Attention\n')
     file.write('Line 1\n')
     file.write('Line 2\n')
     file.writelines(
         ('Line 3\n', 'Line 4\n')
     )
-    file.seek(0, 0)
-    print(file.read())
-    print('Reading')
-    file.seek(0, 0)
-    print(file.readline(), end='')
-    print(file.readline().strip())
-    print(file.readline().strip())
-
-    print('READLINES')
-    file.seek(0, 0)
-    for line in file.readlines():
-        print(line.strip())
-
-
-print('#' * 10)
-
-with open(file_path, 'r') as file:
-    print(file.read())
