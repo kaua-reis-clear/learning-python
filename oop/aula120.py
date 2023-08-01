bfe426fb2821d302efbe2941e48182100ba84af8
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-07-31 21:00:37 -0300

    feat: learned about __init__ method

--- oop/aula120.py
+++ oop/aula120.py
@@ -1,14 +1,19 @@
+# string = 'Kauã'  # str
+# print(string.upper())
+# print(isinstance(string, str))
 class Person:
-    ...
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
 
 
 p1 = Person('Kauã', 'Drar')
-p1.first_name = 'Kauã'
-p1.last_name = 'Drar'
+# p1.first_name = 'Kauã'
+# p1.last_name = 'Drar'
 
 p2 = Person('Vitória', 'Rossi')
-p2.first_name = 'Vitória'
-p2.last_name = 'Rossi'
+# p2.first_name = 'Vitória'
+# p2.last_name = 'Rossi'
 
 print(p1.first_name)
 print(p1.last_name)
