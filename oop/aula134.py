6d0e4036c4833879764ad0bd1f8984edb6195c70
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-09-19 22:46:39 -0300

    feat: learned about association, aggregation and composition

---
+++ oop/aula134.py
@@ -0,0 +1,30 @@
+class Writer:
+    def __init__(self, name) -> None:
+        self.name = name
+        self._tool = None
+
+    @property
+    def tool(self):
+        return self._tool
+
+    @tool.setter
+    def tool(self, tool):
+        self._tool = tool
+
+
+class WritingTool:
+    def __init__(self, name):
+        self.name = name
+
+    def write(self):
+        return f'{self.name} is writing'
+
+
+writer = Writer('Luiz')
+pen = WritingTool('Bic Pen')
+typewriter = WritingTool('Machine')
+writer.tool = typewriter
+
+print(pen.write())
+print(typewriter.write())
+print(writer.tool.write())
